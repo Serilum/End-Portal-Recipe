3e95f8bff4ffd3afc25684c1c79097c105d14bef
Rick South <rick@natamus.com>
2024-07-28 07:35:08 +0200

    (v5.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/endportalrecipe/util/Reference.java
+++ Common/src/main/java/com/natamus/endportalrecipe/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.endportalrecipe.util;
 public class Reference {
 	public static final String MOD_ID = "endportalrecipe";
 	public static final String NAME = "End Portal Recipe";
-	public static final String VERSION = "5.4";
+	public static final String VERSION = "5.5";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
