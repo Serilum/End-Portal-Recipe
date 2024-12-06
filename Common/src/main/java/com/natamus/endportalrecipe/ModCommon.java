d7db7c78c443b1ae3921588027bb4de2040a579b
Rick South <rick@natamus.com>
2024-12-06 13:33:46 +0100

    End Portal Recipe source code for 1.21.4.

---
+++ Common/src/main/java/com/natamus/endportalrecipe/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.endportalrecipe;
+
+import com.natamus.endportalrecipe.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
