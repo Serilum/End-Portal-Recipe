8915db9ab2592fbd3dbdff9791c010ecee65559b
Rick South <rick@natamus.com>
2025-02-12 23:35:43 +0100

    (v5.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/endportalrecipe/neoforge/events/NeoForgeEndPortalEvent.java
+++ NeoForge/src/main/java/com/natamus/endportalrecipe/neoforge/events/NeoForgeEndPortalEvent.java
@@ -5,9 +5,7 @@ import net.minecraft.world.entity.Entity;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeEndPortalEvent {
 	@SubscribeEvent
 	public static void mobItemDrop(LivingDropsEvent e) {
