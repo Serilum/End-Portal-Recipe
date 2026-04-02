69aed83f8e56e22b4073753c94066a15b0caa239
Rick South <rick@natamus.com>
2026-04-02 16:13:42 +0200

    End Portal Recipe source code for 26.1.1.

---
+++ NeoForge/src/main/java/com/natamus/endportalrecipe/neoforge/events/NeoForgeEndPortalEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.endportalrecipe.neoforge.events;
+
+import com.natamus.endportalrecipe.events.EndPortalEvent;
+import net.minecraft.world.entity.Entity;
+import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class NeoForgeEndPortalEvent {
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDropsEvent e) {
+		Entity entity = e.getEntity();
+		EndPortalEvent.mobItemDrop(entity.level(), entity, e.getSource());
+	}
+
+	@SubscribeEvent
+	public static void onLeftClick(PlayerInteractEvent.LeftClickBlock e) {
+		EndPortalEvent.onLeftClick(e.getLevel(), e.getEntity(), e.getPos(), e.getFace());
+	}
+}
