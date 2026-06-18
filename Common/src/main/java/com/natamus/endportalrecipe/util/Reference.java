f49c1e75aafa55de45c4073c6638ed45796b825f
Rick South <rick@natamus.com>
2026-06-18 23:57:20 +0200

    Updated End Portal Recipe to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/endportalrecipe/util/Reference.java
+++ Common/src/main/java/com/natamus/endportalrecipe/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "endportalrecipe";
 	public static final String NAME = "End Portal Recipe";
 	public static final String VERSION = "5.8";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
